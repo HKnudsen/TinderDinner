7457ac4ad22da14abcd111182e18d82e4f11503b
Henrik Knudsen <henrik@csolutions.no>
2021-02-07 17:30:24 +0100

    Initial

---
+++ TinderDinner/TinderDinner/TinderDinner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinderDinner
+//
+//  Created by Henrik Bouwer Knudsen on 07/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
