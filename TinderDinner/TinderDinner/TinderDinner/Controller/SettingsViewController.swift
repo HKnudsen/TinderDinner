53124c8bbed0f644f9cc0b3f3a756ab98e5a7602
Henrik Knudsen <henrik@csolutions.no>
2021-02-08 09:18:37 +0100

    Added UI and connected to VC

---
+++ TinderDinner/TinderDinner/TinderDinner/Controller/SettingsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SettingsViewController.swift
+//  TinderDinner
+//
+//  Created by Henrik Bouwer Knudsen on 08/02/2021.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate      = self
+        tableView.dataSource    = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .gray
+        return cell
+    }
+    
+    
+}
