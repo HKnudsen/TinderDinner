19f6dc4f6cc3de796a34eccdb57c3106bf930470
Henrik Knudsen <henrik@csolutions.no>
2021-02-20 15:50:24 +0100

    Added programatic auto layout for koloda cards

--- TinderDinner/TinderDinner/Controller/CardViewController.swift
+++ TinderDinner/TinderDinner/Controller/CardViewController.swift
@@ -26,7 +26,7 @@ class CardViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the imgView.
         isMultipleUsersSwith.isOn = false
         cardView.delegate = self
         cardView.dataSource = self
@@ -81,35 +81,146 @@ class CardViewController: UIViewController {
             
         }
     }
-    
+    let testIngredients = ["First", "Second", "Third", "Fourth", "Last"]
 }
 
+// MARK: - Koloda Section
 
 extension CardViewController: KolodaViewDelegate, KolodaViewDataSource {
 
     func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
         
+        // Kanskje bruke tableview til ingredients?
+        
         let parentView = UIView(frame: CGRect(x: 0, y: 0, width: cardView.frame.size.width, height: cardView.frame.size.height))
-        let view = UIImageView(image: UIImage(named: "ironman\(index + 1)"))
+        let imgView = UIImageView(image: UIImage(named: "ironman\(index + 1)"))
         
-        let textView = UILabel(frame: CGRect(x: 20, y: parentView.frame.size.height - 70, width: 100, height: 50))
-        textView.text = "Test"
-        textView.textColor = .blue
+        let bottomView: UIView = {
+            let view = UIView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.backgroundColor = .white
+            view.layer.cornerRadius = 20
+            return view
+        }()
         
-        view.frame = parentView.bounds
-        view.frame.size.height = parentView.frame.size.height - 100
-        view.layer.cornerRadius = 20
-        view.clipsToBounds = true
-        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        let stackView: UIStackView = {
+            let view = UIStackView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.axis = .horizontal
+            view.distribution = .fillEqually
+            view.backgroundColor = .cyan
+            view.layer.cornerRadius = 20
+            return view
+        }()
         
+        let leftBottomView: UIView = {
+            let view = UIView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.layer.cornerRadius = 20
+            view.backgroundColor = .blue
+            return view
+        }()
+        
+        let rightBottomView: UIView = {
+            let view = UIView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.layer.cornerRadius = 20
+            view.backgroundColor = .red
+            return view
+        }()
+        
+        let leftList: UIStackView = {
+            let view = UIStackView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.axis = .vertical
+            view.distribution = .fillEqually
+            view.backgroundColor = .brown
+            return view
+        }()
+        
+        let rightList: UIStackView = {
+            let view = UIStackView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.axis = .vertical
+            view.distribution = .fillEqually
+            return view
+        }()
+        
+
+        
+        parentView.addSubview(imgView)
+        parentView.addSubview(bottomView)
+        bottomView.addSubview(stackView)
+        stackView.addArrangedSubview(leftBottomView)
+        stackView.addArrangedSubview(rightBottomView)
+        leftBottomView.addSubview(leftList)
+        rightBottomView.addSubview(rightList)
+        
+        
+        // This enables autolayout for imgView
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+
+        imgView.topAnchor.constraint(equalTo: parentView.topAnchor, constant: 10).isActive = true
+        imgView.leadingAnchor.constraint(equalTo: parentView.leadingAnchor, constant: 10).isActive = true
+        imgView.trailingAnchor.constraint(equalTo: parentView.trailingAnchor, constant: -10).isActive = true
+        imgView.bottomAnchor.constraint(equalTo: bottomView.topAnchor).isActive = true
+        imgView.heightAnchor.constraint(equalToConstant: parentView.frame.size.height * 0.66).isActive = true
+        
+        bottomView.bottomAnchor.constraint(equalTo: parentView.bottomAnchor).isActive = true
+        bottomView.leadingAnchor.constraint(equalTo: parentView.leadingAnchor, constant: 10).isActive = true
+        bottomView.trailingAnchor.constraint(equalTo: parentView.trailingAnchor, constant: -10).isActive = true
+        bottomView.topAnchor.constraint(equalTo: imgView.bottomAnchor).isActive = true
+        
+//        stackView.bottomAnchor.constraint(equalTo: bottomView.bottomAnchor).isActive = true
+//        stackView.leadingAnchor.constraint(equalTo: bottomView.leadingAnchor).isActive = true
+//        stackView.trailingAnchor.constraint(equalTo: bottomView.trailingAnchor).isActive = true
+//        stackView.topAnchor.constraint(equalTo: bottomView.topAnchor).isActive = true
+        
+//        leftList.bottomAnchor.constraint(equalTo: leftBottomView.bottomAnchor).isActive = true
+//        leftList.topAnchor.constraint(equalTo: leftBottomView.topAnchor).isActive = true
+//        leftList.leadingAnchor.constraint(equalTo: leftBottomView.leadingAnchor).isActive = true
+//        leftList.trailingAnchor.constraint(equalTo: leftBottomView.trailingAnchor).isActive = true
+        
+        stackView.centerInParent(parent: bottomView)
+        leftList.centerInParent(parent: leftBottomView)
+        rightList.centerInParent(parent: rightBottomView)
+    
+    
         parentView.layer.cornerRadius = 20
-        parentView.backgroundColor = .white
-        parentView.clipsToBounds = true
         parentView.autoresizesSubviews = true
-        parentView.addSubview(view)
-        parentView.addSubview(textView)
+        imgView.layer.cornerRadius = 20
+        imgView.clipsToBounds = true
         
-        //return parentView
+        for item in testIngredients {
+            let ingredientText: UITextView = {
+                let view = UITextView()
+                view.text = item
+                view.textColor = .green
+                view.textAlignment = .center
+                return view
+            }()
+            
+            rightList.addArrangedSubview(ingredientText)
+            
+    
+        }
+        
+        for item in testIngredients {
+            let ingredientText: UITextView = {
+                let view = UITextView()
+                view.text = item
+                view.textColor = .green
+                view.textAlignment = .center
+                return view
+            }()
+            
+            
+            leftList.addArrangedSubview(ingredientText)
+    
+        }
+        
+
+        
         return parentView
     }
     
@@ -123,7 +234,7 @@ extension CardViewController: KolodaViewDelegate, KolodaViewDataSource {
             guard let items = databaseManager.items else {
                 fatalError("No items found at databaseManager: 118 CardVC")
             }
-            databaseManager.addToWantedDinner(with: items[index])
+            //databaseManager.addToWantedDinner(with: items[index])
         // Dont save
         } else if direction == SwipeResultDirection.left {
             
@@ -141,7 +252,5 @@ extension CardViewController: KolodaViewDelegate, KolodaViewDataSource {
         // Perform segue to results page
         performSegue(withIdentifier: "resultsPage", sender: self)
     }
-    
-    
 }
 
---
+++ TinderDinner/TinderDinner/Extensions/ViewExtenstion.swift
@@ -0,0 +1,20 @@
+//
+//  ViewExtenstion.swift
+//  TinderDinner
+//
+//  Created by Henrik Bouwer Knudsen on 20/02/2021.
+//
+
+import UIKit
+
+extension UIView {
+    func centerInParent(parent: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        topAnchor.constraint(equalTo: parent.topAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: parent.trailingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: parent.bottomAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: parent.leadingAnchor).isActive = true
+    }
+    
+}
